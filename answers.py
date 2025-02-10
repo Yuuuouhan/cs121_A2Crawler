9084fc99f495041a1b01cfc7be2c7cab2111a7d8
Deeksha Dwarakanath <dwarakad@uci.edu>
2025-02-10 02:14:27 -0800

    Preliminary functions for report questions -- need to be inserted into scraper.py

---
+++ answers.py
@@ -0,0 +1,93 @@
+from urllib.parse import urlparse, urldefrag
+import re
+import tokenizer
+
+
+pages = set()
+subdomains = dict()
+max_URL = ""
+max_words = ""
+
+def answers():
+	num_unique_URLS = unique_URLS()
+	longest_page = longest_page()
+	common_words = tokenizer.computeWordFrequencies()
+	subdomains = find_subdomains()
+	
+	with open("final_answers.txt", "w") as file:
+		file.write(f"UNIQUE_URLS: {num_unique_URLS}\n")
+		file.write(f"LONGEST_PAGE: {longest_page}\n")
+		file.write(f"COMMON WORDS: {common_words}\n")
+		file.write(f"SUBDOMAINS: {subdomains}\n")
+
+def add_page(url: str) -> None:
+	"""
+	Checks an incoming URL, removes the fragment, and adds ito the set of pages.
+	
+	@param url: incoming absolute URL path
+	@return: None
+	"""
+	url = urldefrag(url)
+	pages.add(url)
+
+def add_to_ics_domains(url: str) -> None:
+	"""
+	Checks an incoming URL and sees if it is within the ics domain, and whether
+	we should add it to the existing ics_domains set
+	
+	@param url: incoming absolute URL path
+	@return: None
+	"""
+	domain = urlparse(url).netloc
+	url = urldefrag(url)
+	if domain == "ics.uci.edu":
+		parsed_url = urlparse(url).hostname
+		if parsed_url not in subdomains:
+			subdomains[parsed_url] = set(url)
+		else:
+			subdomains[parsed_url].add(url)
+
+def unique_URLS() -> int:
+	"""
+	Returns the number of unique URLS in the ics domain.
+	
+	@return: integer representing the number of unique URLS in the ics domain.
+	"""
+	return len(pages)
+
+def update_max_URL(url: str, response: str) -> None:
+	"""
+	Updates max_URL and max_words to find the longest page in terms of the number
+	of words.
+	
+	@param response: takes the writeup from an incoming URL.
+	@return: None
+	"""
+	string = re.compile(r'<\/?[a-z][\s\S]*?>', re.IGNORECASE)
+	is_HTML = bool(string.match(response))
+	if not is_HTML:
+		new_length = len(response)
+		if max_words < new_length:
+			max_URL = url
+			max_words = new_length
+
+def longest_page() -> int:
+	"""
+	Returns the longest page in terms of the number of words, excluding HTML markup.
+	
+	@return: integer representing the longest page in terms of the number of words.
+	"""
+	return max_words
+
+def find_subdomains() -> dict:
+	"""
+	Returns the subdomains of ics.uci.edu, as well as the number of unique pages
+	belonging to each subdomain.
+	
+	@return: dictionary of each subdomain and its respective number of unique pages.
+	"""
+	return_dict = dict()
+	for subdomain in subdomains:
+		return_dict[subdomain] = len(subdomains[subdomain])
+	return return_dict
+
