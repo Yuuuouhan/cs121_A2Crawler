711f10b23d1d0d9d54701d7064bdd1a8f8eea509
Pragya Jhunjhunwala <pjhunjh1@uci.edu>
2025-02-10 17:20:03 -0800

    new file to store all beautiful soup functionality code

---
+++ beautifulsoup.py
@@ -0,0 +1,8 @@
+#file to store all functionality done by beautifulsoup
+
+from bs4 import BeautifulSoup
+import re
+from urllib.parse import urlparse
+from urllib.parse import urljoin, urldefrag
+import time
+
