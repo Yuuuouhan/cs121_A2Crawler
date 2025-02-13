55c0b4689cabc343c2d9f9bba58258e7d00ad1cb
Helena Sun <helenays@uci.edu>
2025-02-13 14:29:56 -0800

    Intergrated simhash and checksum to scraper

--- duplication.py
+++ duplication.py
@@ -3,38 +3,31 @@ import hashlib
 # hard code things to avoid
 # unique pages and filtered
 
-def get_frequencies(tokens:list):
+def checksum(tokens:list):
     """
-    Get the frequencies of each tokens to use as weights.
-
-    @param: list of tokens
-    @return: diction of token to frequency
+    Calculates the checksum of a list of tokens using MD5. Order of tokens not considered.
+    @param: list of tokens (string)
+    @return: integer value of checksum
     """
-    freq = {}
-    for token in tokens:
-        if token in freq.keys():
-            freq[token] += 1
-        else:
-            freq[token] = 1
-    return freq
+    hashed_toks = [int(hashlib.md5(token.encode('utf-8')).hexdigest(), 16) for token in tokens]
+    return sum(hashed_toks)
 
 
-def simhash(tokens:list):
+def simhash(tok_weights:dict):
     """
-    Calculates the 64 bit simhash given a list of tokens using MD5.
+    Calculates the 64 bit simhash given a dictionary of token: frequency using MD5.
 
-    @param: list of tokens
+    @param: dictionary of token: frequency
     @return: integer representation of 64 bit simhash of the tokens
     """
-    weights = get_frequencies(tokens)
     hash_values = [0] * 64
-    for token in weights.keys():
+    for token in tok_weights.keys():
         hash_val = bin(int.from_bytes(hashlib.md5(token.encode('utf-8')).digest()))[2:66] # bit string
         for i in range(64):
             if hash_val[i] == '0':
-                hash_values[i] -= weights[token]
+                hash_values[i] -= tok_weights[token]
             else:
-                hash_values[i] += weights[token]
+                hash_values[i] += tok_weights[token]
     simhash_val = ["0" if bit <= 0 else "1" for bit in hash_values]
     return bitvector_to_int(simhash_val)
 
@@ -66,6 +59,7 @@ def similarity_score(sim1:int, sim2:int):
 
 if __name__ == "__main__":
     tokens1 = ["Hello", "Deeksha", "and", "Pragya"]
+    tokens15 = ["Deeksha", "Hello", "and", "Pragya"]
     tokens2 = ["Hello", "Deeksha", "and", "Pragya", "and", "Helena"]
     tokens3 = ["Bye", "for", "now", "hahahaha", "more", "tokens"]
     sim1 = simhash(tokens1)
@@ -77,3 +71,6 @@ if __name__ == "__main__":
     print(similarity_score(sim1, sim2))
     print(similarity_score(sim1, sim3))
     print(similarity_score(sim2, sim3))
+    print(checksum(tokens1))
+    print(checksum(tokens15))
+    print(checksum(tokens2))
